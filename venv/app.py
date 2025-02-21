b03c57857fe204769ab384883f94a1406ee28ac6
DORNALA GOPIREDDY <140056529+dornalagopireddy@users.noreply.github.com>
2025-02-21 19:00:05 +0530

    Update app.py

--- venv/app.py
+++ venv/app.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template, request
-
+import os
 app = Flask(__name__)
 
 @app.route("/", methods=["GET", "POST"])
@@ -22,6 +22,6 @@ def index():
             category = "Obesity"
 
     return render_template("index.html", bmi=bmi, category=category)
-
+port=int(os.environ.get("PORT",10000)
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host="0.0.0.0",port=port)
